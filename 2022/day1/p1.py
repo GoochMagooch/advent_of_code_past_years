4c7e3bfc54a778d032b8cda6c32b4505ab98bad8
Magooch <crvallecito@gmail.com>
2024-12-09 19:52:34 -0700

    Solved part 1 yay

--- 2022/day1/p1.py
+++ 2022/day1/p1.py
@@ -1,4 +1,27 @@
 with open("input.txt") as input:
-    data = input.read()
+    data = input.readlines()
 
-print(data)
+data_lines = []
+lines = []
+for line in data:
+    lines.append(line)
+    if line == "\n":
+        data_lines.append(lines)
+        lines = []
+
+new_data_lines = []
+for i in data_lines:
+    new_lines = []
+    for j in i:
+        new_lines.append(j.strip())
+    new_data_lines.append(new_lines)
+
+counter = 0
+for line in new_data_lines:
+    sum = 0
+    for element in line:
+        if element.isdigit():
+            sum += int(element)
+    if sum > counter:
+        counter = sum
+print(counter)
