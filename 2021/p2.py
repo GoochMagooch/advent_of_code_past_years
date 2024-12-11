ce85e051e3b380035d0d4c26b40206dd708ea3ec
Magooch <crvallecito@gmail.com>
2024-12-10 17:08:20 -0700

    Create AoC/2021/day1/pt2 source file

---
+++ 2021/p2.py
@@ -0,0 +1,16 @@
+with open("input.txt") as input:
+    data = input.readlines()
+
+data_ints = []
+for line in data:
+    data_ints.append(int(line.strip()))
+
+incr = 0
+decr = 0
+
+for i in range(1, len(data_ints)):
+    if data_ints[i] > data_ints[i - 1]:
+        incr += 1
+    elif data_ints[i] < data_ints[i - 1]:
+        decr += 1
+
