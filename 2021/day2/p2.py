a04fbaaa11a25a3c4985d6fc8fffe3660513bad3
Magooch <crvallecito@gmail.com>
2024-12-10 18:36:32 -0700

    Calculate aim, as well as horizon * depth

--- 2021/day2/p2.py
+++ 2021/day2/p2.py
@@ -2,21 +2,21 @@ with open("input.txt") as input:
     data = input.readlines()
 
 depth = 0
+aim = 0
 horizon = 0
 for line in data:
     if "down" in line:
         for i in line:
             if i.isdigit():
-                depth += int(i)
+                aim += int(i)
     elif "up" in line:
         for i in line:
             if i.isdigit():
-                depth -= int(i)
+                aim -= int(i)
     elif "forward" in line:
         for i in line:
             if i.isdigit():
                 horizon += int(i)
+                depth += aim * int(i)
 
-print("Depth: " + str(depth))
-print("Horizon: " + str(horizon))
-print(depth * horizon)
+print(horizon * depth)
