cdd65161b4e74b7e4a81992be98f3dafdae4c706
Magooch <crvallecito@gmail.com>
2024-12-11 13:56:47 -0700

    Saving code for later

--- 2021/day3/p1.py
+++ 2021/day3/p1.py
@@ -1,3 +1,25 @@
 with open("input.txt") as input:
-    data = input.read()
+    data = input.readlines()
 
+rows = []
+for line in data:
+    lines = []
+    for i in line.strip():
+        lines.append(int(i))
+    rows.append(lines)
+
+final_bin = []
+for i in range(len(rows[0])):
+    zero_bit = 0
+    one_bit = 0
+    for j in range(len(rows)):
+        if j == 0:
+            zero_bit += 1
+        else:
+            one_bit += 1
+    if one_bit > zero_bit:
+        final_bin.append(1)
+    else:
+        final_bin.append(0)
+
+print(final_bin)
