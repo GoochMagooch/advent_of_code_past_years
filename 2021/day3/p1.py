d0b2bd6f3e279dabf959a6fd0cc4c74853e3d4ef
Magooch <crvallecito@gmail.com>
2024-12-11 15:14:59 -0700

    Create function to convert binary values to decimal values

--- 2021/day3/p1.py
+++ 2021/day3/p1.py
@@ -8,7 +8,8 @@ for line in data:
         lines.append(int(i))
     rows.append(lines)
 
-final_bin = []
+final_bin_gamma = []
+final_bin_epsilon = []
 for i in range(len(rows[0])):
     zero_bit = 0
     one_bit = 0
@@ -18,11 +19,35 @@ for i in range(len(rows[0])):
         else:
             one_bit += 1
     if one_bit > zero_bit:
-        final_bin.append(1)
+        final_bin_gamma.append(1)
+        final_bin_epsilon.append(0)
     else:
-        final_bin.append(0)
+        final_bin_gamma.append(0)
+        final_bin_epsilon.append(1)
+
 
 gamma = ''
-for i in final_bin:
+for i in final_bin_gamma:
     gamma += str(i)
-print(gamma)
+epsilon = ''
+for i in final_bin_epsilon:
+    epsilon += str(i)
+
+def binary_decoder(string_of_ints):
+    lst = []
+    for i in string_of_ints:
+        lst.append(int(i))
+
+    char = 1
+    decimal = 0
+    for i in range(len(lst)):
+        if lst[-1] == 1:
+            decimal += char
+            char *= 2
+            lst.pop()
+        else:
+            char *= 2
+            lst.pop()
+    return decimal
+
+print(binary_decoder(gamma))
