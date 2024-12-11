44ff6203ea7f3065eef4ba2a7a5e2815ce14e7e1
Magooch <crvallecito@gmail.com>
2024-12-10 17:51:26 -0700

    Create AoC/2021/day2/pt2 source file

---
+++ 2021/day2/p2.py
@@ -0,0 +1,22 @@
+with open("input.txt") as input:
+    data = input.readlines()
+
+depth = 0
+horizon = 0
+for line in data:
+    if "down" in line:
+        for i in line:
+            if i.isdigit():
+                depth += int(i)
+    elif "up" in line:
+        for i in line:
+            if i.isdigit():
+                depth -= int(i)
+    elif "forward" in line:
+        for i in line:
+            if i.isdigit():
+                horizon += int(i)
+
+print("Depth: " + str(depth))
+print("Horizon: " + str(horizon))
+print(depth * horizon)
