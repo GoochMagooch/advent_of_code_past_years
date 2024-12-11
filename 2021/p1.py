e604325e883587fcd50303a933aa96292f9c0cfb
Magooch <crvallecito@gmail.com>
2024-12-10 17:03:10 -0700

    Finish logic to check if each element of data_ints is gt or lt the last

--- 2021/p1.py
+++ 2021/p1.py
@@ -1,4 +1,18 @@
 with open("input.txt") as input:
-    data = input.read()
+    data = input.readlines()
 
-print(data)
+data_ints = []
+for line in data:
+    data_ints.append(int(line.strip()))
+
+incr = 0
+decr = 0
+
+for i in range(1, len(data_ints)):
+    if data_ints[i] > data_ints[i - 1]:
+        incr += 1
+    elif data_ints[i] < data_ints[i - 1]:
+        decr += 1
+
+print(incr)
+print(decr)
