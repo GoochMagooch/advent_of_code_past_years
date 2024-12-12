01f8e70db775e4ec6ef128cca81c80166f96004a
Magooch <crvallecito@gmail.com>
2024-12-11 22:53:09 -0700

    Calculate with elements in lines, when added together equal 2020, and multiply the two elements

--- 2020/day1/p1.py
+++ 2020/day1/p1.py
@@ -1,3 +1,17 @@
 with open("input.txt") as input:
     data = input.readlines()
 
+lines = []
+for line in data:
+    lines.append(int(line.strip()))
+
+ans = 0
+for i in lines:
+    iterator = []
+    iterator.append(i)
+    for j in lines:
+        j_iterator = []
+        if j + iterator[0] == 2020:
+            ans = j * i
+
+print(ans)
