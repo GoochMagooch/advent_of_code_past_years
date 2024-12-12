752c07df0e8eccbe91fc8899b5e997865dcedaec
Magooch <crvallecito@gmail.com>
2024-12-11 22:55:40 -0700

    Calculate which 3 elements in lines add up to 2020 and multiply those elements

---
+++ 2020/day1/p2.py
@@ -0,0 +1,20 @@
+with open("input.txt") as input:
+    data = input.readlines()
+
+lines = []
+for line in data:
+    lines.append(int(line.strip()))
+
+ans = 0
+for i in lines:
+    iterator = []
+    iterator.append(i)
+    for j in lines:
+        j_iterator = []
+        j_iterator.append(j)
+        for k in lines:
+            if k + j_iterator[0] + iterator[0] == 2020:
+                ans = k * j * i
+
+print(ans)
+
