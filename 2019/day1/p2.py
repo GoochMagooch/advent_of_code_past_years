05ecc7026dbe33920e89716015f94b83466d4ae7
Magooch <crvallecito@gmail.com>
2024-12-16 17:42:03 -0700

    Refactor p1.py to find necessary fuel for each subsequent fuel level

--- 2019/day1/p2.py
+++ 2019/day1/p2.py
@@ -6,12 +6,16 @@ with open("input.txt") as i:
     d = i.readlines()
 data = [int(line.strip()) for line in d]
 
-# iterates through a list of modules and calculates total fuel needed
+# iterates through a list of modules and finds necessary fuel to run that module
+# then finds the fuel to run that fuel, and so on until it reaches close to 0
 def find_fuel(modules):
     fuel_needed = 0
     for i in modules:
-        fuel_needed += math.floor(i / 3 - 2)
+        counter = math.floor(i / 3 - 2)
+        while counter > 0:
+            fuel_needed += counter
+            counter = math.floor(counter / 3 - 2)
     return fuel_needed
 
-# answer for AoC/2019/day1/pt1
+# answer for AoC/2019/day1/pt2
 print(find_fuel(data))
