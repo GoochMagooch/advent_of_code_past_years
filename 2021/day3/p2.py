6c33f9d4fa6c88021f79b282e6c9a573944e9a90
Magooch <crvallecito@gmail.com>
2025-01-07 13:11:36 -0700

    Add binary decoder and create new variables to work with

--- 2021/day3/p2.py
+++ 2021/day3/p2.py
@@ -1,4 +1,4 @@
-with open("example.txt") as input:
+with open("input.txt") as input:
     data = input.readlines()
 
 rows = []
@@ -7,3 +7,79 @@ for line in data:
     for i in line.strip():
         lines.append(int(i))
     rows.append(lines)
+
+# calculates the number of 1's and 0's in each column of `rows`
+# places those values in their respective categories of either 'gamma' or 'epsilon'
+final_bin_gamma = []
+final_bin_epsilon = []
+for i in range(len(rows[0])):
+    zero_bit = 0
+    one_bit = 0
+    for j in range(len(rows)):
+        if rows[j][i] == 0:
+            zero_bit += 1
+        else:
+            one_bit += 1
+    if one_bit > zero_bit:
+        final_bin_gamma.append(1)
+        final_bin_epsilon.append(0)
+    else:
+        final_bin_gamma.append(0)
+        final_bin_epsilon.append(1)
+
+# converts gamma and epsilon lists to strings of digits
+gamma = ''
+for i in final_bin_gamma:
+    gamma += str(i)
+epsilon = ''
+for i in final_bin_epsilon:
+    epsilon += str(i)
+
+# converts strings of binary to the decimal value
+def binary_decoder(string_of_ints):
+    lst = []
+    for i in string_of_ints:
+        lst.append(int(i))
+
+    char = 1
+    decimal = 0
+    for i in range(len(lst)):
+        if lst[-1] == 1:
+            decimal += char
+            char *= 2
+            lst.pop()
+        else:
+            char *= 2
+            lst.pop()
+    return decimal
+
+power_consumption = binary_decoder(gamma) * binary_decoder(epsilon)
+
+ox_gen_rating = 0
+c02_scrub_rating = 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
