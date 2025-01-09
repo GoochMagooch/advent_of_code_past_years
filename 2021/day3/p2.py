e01f466fd150321405c5bb7995d2180d70dcf23c
Magooch <crvallecito@gmail.com>
2025-01-08 18:09:26 -0700

    More attempts

--- 2021/day3/p2.py
+++ 2021/day3/p2.py
@@ -1,41 +1,48 @@
 with open("input.txt") as input:
     data = input.readlines()
 
-rows = []
-for line in data:
-    lines = []
-    for i in line.strip():
-        lines.append(int(i))
-    rows.append(lines)
+rows = [line.strip() for line in data]
 
-# calculates the number of 1's and 0's in each column of `rows`
-# places those values in their respective categories of either 'gamma' or 'epsilon'
-final_bin_gamma = []
-final_bin_epsilon = []
-for i in range(len(rows[0])):
-    zero_bit = 0
-    one_bit = 0
-    for j in range(len(rows)):
-        if rows[j][i] == 0:
-            zero_bit += 1
-        else:
-            one_bit += 1
-    if one_bit > zero_bit:
-        final_bin_gamma.append(1)
-        final_bin_epsilon.append(0)
-    else:
-        final_bin_gamma.append(0)
-        final_bin_epsilon.append(1)
+# if there are more 0's than 1's in this column then do this
+# else do this
 
-# converts gamma and epsilon lists to strings of digits
-gamma = ''
-for i in final_bin_gamma:
-    gamma += str(i)
-epsilon = ''
-for i in final_bin_epsilon:
-    epsilon += str(i)
+ox_lst = []
+c02_lst = []
+for i in range(len(rows)):
+    if rows[i][0] == '1':
+        ox_lst.append(rows[i])
+    elif rows[i][0] == '0':
+        c02_lst.append(rows[i])
 
-# converts strings of binary to the decimal value
+# find the most common digit in each row
+counter = 0
+while len(ox_lst) > 1:
+    ox_final = []
+    for i in range(len(ox_lst)):
+        if ox_lst[i][counter] == '1':
+            ox_final.append(ox_lst[i])
+    ox_lst = ox_final
+    counter += 1
+print(ox_lst)
+# finds the least common digit in each row
+counter = 0
+while len(c02_lst) > 1:
+    c02_final = []
+    for i in range(len(c02_lst)):
+        if c02_lst[i][counter] == '0':
+            c02_final.append(c02_lst[i])
+    c02_lst = c02_final
+    counter += 1
+print(c02_lst)
+
+oxygen = ''
+for i in ox_lst:
+    oxygen += str(i)
+c02 = ''
+for i in c02_lst:
+    c02 += str(i)
+
+
 def binary_decoder(string_of_ints):
     lst = []
     for i in string_of_ints:
@@ -53,35 +60,5 @@ def binary_decoder(string_of_ints):
             lst.pop()
     return decimal
 
-power_consumption = binary_decoder(gamma) * binary_decoder(epsilon)
-print(power_consumption)
-
-ox_gen_rating = 0
-c02_scrub_rating = 0
-life_support = ox_gen_rating * c02_scrub_rating
-print(life_support)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(binary_decoder(oxygen))
+print(binary_decoder(c02))
