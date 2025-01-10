baad5643e114e5fd77749720e9688e5a627af092
Magooch <crvallecito@gmail.com>
2025-01-10 12:41:01 -0700

    Something or another

--- 2021/day3/p2.py
+++ 2021/day3/p2.py
@@ -1,19 +1,20 @@
-with open("input.txt") as input:
+with open("example.txt") as input:
     data = input.readlines()
 
 rows = [line.strip() for line in data]
-ox_lst = []
-ones = []
-zeros = []
-counter = 0
-
 while len(rows) > 1:
-    for i in range(len(rows)):
-        if rows[i][counter] == '1':
-            ones.append(rows[i])
-        elif rows[i][counter] == '0':
-            zeros.append(rows[i])
-    counter += 1
+    ox_lst = []
+    ones = []
+    zeros = []
+    # counter reinitializes on each while iteration
+    counter = 0
+    for i in range(len(rows[0])):
+        for j in range(len(rows)):
+            if rows[j][counter] == '1':
+                ones.append(rows[i])
+            elif rows[j][counter] == '0':
+                zeros.append(rows[i])
+    counter += 1        
 
     if len(ones) == len(zeros):
         ox_lst = ones
@@ -23,9 +24,7 @@ while len(rows) > 1:
         ox_lst = zeros
         
     rows = ox_lst
-    if len(rows) == 1:
-        ox_lst = rows[0]
-        continue
+ox_lst = rows
 print(ox_lst)
 
 '''
