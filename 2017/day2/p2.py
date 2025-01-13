10b1c8ea7c0ea9dc54078cd931a605c2d419967f
Magooch <crvallecito@gmail.com>
2025-01-12 18:55:27 -0700

    Finish logic for dividing rows and checking for whole numbers

---
+++ 2017/day2/p2.py
@@ -0,0 +1,32 @@
+with open("test.txt") as x:
+    y = x.readlines()
+
+# series of data cleaning
+d = [line.replace('\t', ' ') for line in y]
+a = [i.strip() for i in d]
+t = []
+for i in a:
+    t.append(i.split(' '))
+
+# create list named 'data' to manipulate
+data = []
+for i in t:
+    ints = []
+    for item in i:
+        ints.append(int(item))
+    data.append(ints)
+
+print(data)
+
+final_num = 0
+for i in range(len(data)):
+    placeholder = data[i]
+    for item in placeholder:
+        divisor = item
+        counter = 0
+        for j in range(len(placeholder)):
+            num = divisor / placeholder[counter]
+            if num.is_integer() and num > 1:
+                final_num += num
+            counter += 1
+print(final_num)
