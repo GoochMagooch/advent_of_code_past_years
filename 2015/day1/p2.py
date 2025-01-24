711872ac153d7c4d1792639c6bb03de680f37fce
Magooch <crvallecito@gmail.com>
2025-01-23 22:02:17 -0700

    Found correct answer

--- 2015/day1/p2.py
+++ 2015/day1/p2.py
@@ -1,4 +1,18 @@
 with open("input.txt") as y:
-    x = y.read()
+    x = y.read().strip()
 
-print(x)
+data = []
+for i in x:
+    data.append(i)
+
+index = 0
+counter = 0
+for i in data:
+    if i == '(':
+        counter += 1
+        index += 1
+    elif i == ')':
+        counter -= 1
+        index += 1
+    if counter == -1:
+        print(index)
