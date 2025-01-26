eb576bf3c4300ed54c9721417693e97901d9ba9f
Magooch <crvallecito@gmail.com>
2025-01-26 01:49:50 -0700

    Remove unnecessary characters and whitespace from each line of input

--- 2023/day2/p1.py
+++ 2023/day2/p1.py
@@ -1,9 +1,20 @@
+import re
 with open("example.txt") as y:
     x = y.readlines()
 
 data = []
-for i in x:
-    data.append(i.strip().replace('Game ', ''))
-print(data)
+for i in range(len(x)):
+    data.append(x[i].replace(f'Game {i+1}: ', ''))
 
 id_sum = 0
+games = []
+for i in data:
+    games.append(i.split(';'))
+
+games_stripped = []
+for i in games:
+    sublist = []
+    games_stripped.append(sublist)
+    for j in i:
+        sublist.append(j.strip())
+print(games_stripped)
