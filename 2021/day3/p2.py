4ea49646beb963b547871f682de63d7272947212
Magooch <crvallecito@gmail.com>
2025-03-12 16:04:06 -0600

    Fuck

--- 2021/day3/p2.py
+++ 2021/day3/p2.py
@@ -2,6 +2,52 @@ with open("example.txt") as input:
     data = input.readlines()
 
 rows = [line.strip() for line in data]
+
+'''
+ones = []
+counter = 0
+for i in rows:
+    temp_lst = []
+    if int(i[counter]) == 1:
+        temp_lst.append(int(i))
+    else:
+        continue
+    if len(temp_lst) == 1:
+        ones.append(temp_lst)
+        break
+    counter += 1
+print(ones)
+'''
+
+def find_ones(lst):
+    ones = [-1, -2]
+    while len(ones) > 1:
+        counter = 0
+        for i in lst:
+            if lst[counter] == 1:
+                ones.append(f"{counter}: {lst[i]}")
+            else:
+                continue
+        counter += 1
+        for item in ones:
+            if int(item[counter - 1][0]) < counter:
+                ones.remove(ones[item])
+        if len(ones) == 1:
+            ones.append(ones)
+        return ones
+print(find_ones(rows))
+
+'''
+int_lst = []
+for i in rows:
+    temp_lst = []
+    for j in i:
+        temp_lst.append(int(j))
+    int_lst.append(temp_lst)
+print(int_lst)
+'''
+
+'''
 while len(rows) > 1:
     ox_lst = []
     ones = []
@@ -26,3 +72,4 @@ while len(rows) > 1:
     rows = ox_lst
 ox_lst = rows
 print(ox_lst)
+'''
