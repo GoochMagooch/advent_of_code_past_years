e8a1f8fdadadec4e42ee10a51f84b4d379af8117
Magooch <crvallecito@gmail.com>
2025-03-13 14:27:43 -0600

    Separate integers and operators into separate lists, iterate through int list and perform appropriate expressions

--- 2018/day1/p1.py
+++ 2018/day1/p1.py
@@ -1,10 +1,40 @@
-with open("test.txt") as x:
+with open("input.txt") as x:
     y = x.readlines()
 
 data = [line.strip() for line in y]
 
-lst = []
+data_separated = []
 for i in data:
-    placeholder_lst = []
+    temp = []
     for j in i:
-        placeholder_lst.append()
+        temp.append(j)
+    data_separated.append(temp)
+
+int_data = []
+operators = []
+for i in data_separated:
+    counter = 0
+    temp = []
+    integers = ''
+    for j in i:
+        if counter == 0:
+            operators.append(i[0])
+            counter += 1
+        else:
+            integers += i[counter]
+            counter += 1
+    integers.join('')
+    int_data.append(int(integers))
+print(int_data)
+print(operators)
+
+index = 0
+frequency = 0
+for i in operators:
+    if i == '+':
+        frequency += int_data[index]
+        index += 1
+    else:
+        frequency -= int_data[index]
+        index += 1
+print(frequency)
