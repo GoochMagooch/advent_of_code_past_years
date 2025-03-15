a3564f5db715cf660ba082cbb275cb8c36ab14d2
Magooch <crvallecito@gmail.com>
2025-03-14 19:53:03 -0600

    asdlkfjas;ldkfjasdl

--- 2022/day2/p2.py
+++ 2022/day2/p2.py
@@ -3,39 +3,26 @@ with open("input.txt") as y:
 
 data = [i.strip().replace(' ', '') for i in d]
 
-p_one = []
-p_two = []
+lst_one = []
+lst_two = []
 for i in data:
-    p_one.append(i[0])
-    p_two.append(i[1])
+    lst_one.append(i[0])
+    lst_two.append(i[1])
 
-def game(lst1, lst2):
-    x = 1
-    y = 2
-    z = 3
+def game(lst):
+    point = 1
+    draw = 3
     win = 6
     points = 0
-    for i in range(len(lst1)):
-        if lst1[i] == 'A' and lst2[i] == 'X':
-                points += x + 3
-        elif lst1[i] == 'B' and lst2[i] == 'Y':
-                points += y + 3
-        elif lst1[i] == 'C' and lst2[i] == 'Z':
-                points += z + 3
-        elif lst1[i] == 'A' and lst2[i] == 'Y':
-            points += win + y
-        elif lst1[i] == 'A' and lst2[i] == 'Z':
-            points += z
-        elif lst1[i] == 'B' and lst2[i] == 'X':
-            points += x
-        elif lst1[i] == 'B' and lst2[i] == 'Z':
-            points += win + z
-        elif lst1[i] == 'C' and lst2[i] == 'X':
-            points += win + x
-        elif lst1[i] == 'C' and lst2[i] == 'Y':
-            points += y
+    for i in range(len(lst)):
+        if lst[i] == 'X':
+                points += point
+        elif lst[i] == 'Y':
+                points += point + draw
+        else:
+                points += point + win
     return points
 
-answer = game(p_one, p_two)
+answer = game(lst_two)
 
 print(f"Answer: {answer}")
