d1f1fb775df0d8e7907f626f05b393a06cf12f43
Magooch <crvallecito@gmail.com>
2025-03-16 16:44:13 -0600

    Create comments for readability

--- 2019/day4/p1.py
+++ 2019/day4/p1.py
@@ -1,4 +1,10 @@
+# Reads data from input.txt and splits data by hyphen (-)
 with open('input.txt') as x:
-    data = x.readlines()
+    data = x.read()
+ranges = data.split('-')
 
-print(data)
+# Assigns ranges to 'range_one' and 'range_two'
+range_one = int(ranges[0])
+range_two = int(ranges[1])
+
+# 
