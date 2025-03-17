b524c33b0917d7c6f71139cf0b40238ff18cc17c
Magooch <crvallecito@gmail.com>
2025-03-16 18:36:28 -0600

    Got up to the conditions to check if passwords are valid

--- 2019/day4/p1.py
+++ 2019/day4/p1.py
@@ -1,10 +1,24 @@
-# Reads data from input.txt and splits data by hyphen (-)
+# Reads data from input.txt
 with open('input.txt') as x:
-    data = x.read()
-ranges = data.split('-')
+    # Assigns range limits to 'range_one' and 'range_two' as integers
+    range_one, range_two = map(int, x.read().split('-'))
 
-# Assigns ranges to 'range_one' and 'range_two'
-range_one = int(ranges[0])
-range_two = int(ranges[1])
+# Iterates through range and creates a list of string ranges to iterate through
+range_strings = []
+for i in range(range_one, range_two + 1):
+    range_strings.append(str(i))
 
-# 
+# Iterates through range_strings and creates individual lists of ranges, as integers, separated
+range_list = []
+for i in range_strings:
+    temp = []
+    for j in i:
+        temp.append(int(j))
+    range_list.append(temp)
+print(range_list)
+
+# Checks if conditions are true for each password
+ans = 0
+for i in range_list:
+    for i in i:
+        pass
