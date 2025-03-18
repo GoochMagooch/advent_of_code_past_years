975b8d5599c71327745bab320096022601831450
Magooch <crvallecito@gmail.com>
2025-03-17 18:28:46 -0600

    Finish part 1

--- 2021/day7/p1.py
+++ 2021/day7/p1.py
@@ -1,9 +1,27 @@
+import sys
 with open('input.txt') as x:
     data_first = x.read().split(',')
-print(data_first)
 
 data = []
 for i in data_first:
     data.append(int(i))
-print(data)
 
+position = 1
+lowest_fuel = sys.maxsize
+lowest_position = 0
+    
+for i in range(len(data)):
+    fuel = 0
+    for j in data:
+        difference = 0
+        if j >= position:
+            difference = j - position
+        else:
+            difference = position - j
+        fuel += difference
+    if fuel < lowest_fuel:
+        lowest_fuel = fuel
+        lowest_position = position
+    position += 1
+print(lowest_fuel)
+print(lowest_position)
