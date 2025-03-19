2a6e67880f0817f043aaad611442f7f22f3ba6c5
Magooch <crvallecito@gmail.com>
2025-03-18 22:39:14 -0600

    Create python source code for part 2

---
+++ 2016/day6/p2.py
@@ -0,0 +1,37 @@
+with open('input.txt') as x:
+    y = x.readlines()
+
+data = []
+for i in y:
+    data.append(i.strip())
+
+columns = []
+for i in range(len(data[0])):
+    temp = []
+    for j in range(len(data)):
+        temp.append(data[j][i])
+    columns.append(temp)
+
+unique_chars = []
+for i in columns:
+    temp = []
+    for j in i:
+        if not j in temp:
+            temp.append(j)
+    unique_chars.append(temp)
+
+ans = ''
+for i in range(len(unique_chars)):
+    most_common = 0
+    char = ''
+    for j in unique_chars[i]:
+        temp_int = 0
+        for k in range(len(columns[i])):
+            if j == columns[i][k]:
+                temp_int += 1
+        if temp_int > most_common:
+            most_common = temp_int
+            char = j
+    ans += char
+
+print(ans)
