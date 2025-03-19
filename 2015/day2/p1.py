5f9cc9088611cc7917bdc000f6bd93cc0589d39e
Magooch <crvallecito@gmail.com>
2025-03-19 10:36:23 -0600

    First iteration of function to count total dimensions in fett

--- 2015/day2/p1.py
+++ 2015/day2/p1.py
@@ -1,4 +1,36 @@
-with open("input.txt") as y:
-    x = y.read()
+import sys
+import csv
+with open("input.csv") as y:
+    x = csv.reader(y, delimiter='x')
+    l = []
+    w = []
+    h = []
+    for i in x:
+        l.append(int(i[0]))
+        w.append(int(i[1]))
+        h.append(int(i[2]))
 
-print(x)
+# 2*l*w + 2*w*h + 2*h*l
+dimensions = []
+for i in range(len(l)):
+    count = 0
+    min_side = sys.maxsize
+    temp = []
+    side1 = 2 * l[i] * w[i]
+    count += 1
+    temp.append(side1)
+    side2 = 2 * w[i] * h[i]
+    count += 1
+    temp.append(side2)
+    side3 = 2 * h[i] * l[i]
+    count += 1
+    temp.append(side3)
+    for i in range(count):
+        if temp[i] < min_side:
+            min_side = i
+    dimensions.append(side1 + side2 + side3 + min_side)
+
+ans = 0
+for i in dimensions:
+    ans += i
+print(ans)
