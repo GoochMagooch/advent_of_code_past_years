567ca3e042de5e28b6c0db69ea0655836f19fb79
Magooch <crvallecito@gmail.com>
2025-03-18 22:51:50 -0600

    Formatting

--- 2016/day6/p2.py
+++ 2016/day6/p2.py
@@ -1,3 +1,4 @@
+import sys
 with open('input.txt') as x:
     y = x.readlines()
 
@@ -22,16 +23,15 @@ for i in columns:
 
 ans = ''
 for i in range(len(unique_chars)):
-    most_common = 0
+    least_common = sys.maxsize
     char = ''
     for j in unique_chars[i]:
         temp_int = 0
         for k in range(len(columns[i])):
             if j == columns[i][k]:
                 temp_int += 1
-        if temp_int > most_common:
-            most_common = temp_int
+        if temp_int < least_common:
+            least_common = temp_int
             char = j
     ans += char
-
 print(ans)
