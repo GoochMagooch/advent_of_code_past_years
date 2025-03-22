57af33fdadabb848cd14c660235300db60c1ad2d
Magooch <crvallecito@gmail.com>
2025-03-22 14:34:17 -0600

    Finish part 1

--- 2015/day5/p1.py
+++ 2015/day5/p1.py
@@ -1,19 +1,21 @@
-with open('test.txt') as y:
+with open('input.txt') as y:
     data = [i.strip() for i in y]
 
 vowels = ['a', 'e', 'i', 'o', 'u']
-nice = 0
+banned = ['ab', 'cd', 'pq', 'xy']
+ans = 0
 for i in range(len(data)):
-    count = 0
-    '''
+    nice = True
+    
     # Counts vowels in each string
     vowel_count = 0
     for j in data[i]:
         if j in vowels:
             vowel_count += 1
-    if vowel_count >= 3:
-        count += 1
-    '''
+    if vowel_count < 3:
+        nice = False
+        continue
+
     # Checks if string contains at least one set of repeating characters
     index = -1
     same_char_count = 0
@@ -21,7 +23,30 @@ for i in range(len(data)):
         if data[i][index] == data[i][index - 1]:
             same_char_count += 1
         index -= 1
-    if same_char_count > 0:
-        count += 1
+    if same_char_count == 0:
+        nice = False
+        continue
+    
+    # Checks if string does not contain 'ab', 'cd, 'pq', or 'xy'
+    index = -1
+    # for j in range(4):
+    for k in range((len(data[i])) - 1):
+        if data[i][index] == 'b' and data[i][index - 1] == 'a':
+            nice = False
+            break
+        elif data[i][index] == 'd' and data[i][index - 1] == 'c':
+            nice = False
+            break
+        elif data[i][index] == 'q' and data[i][index - 1] == 'p':
+            nice = False
+            break
+        elif data[i][index] == 'y' and data[i][index - 1] == 'x':
+            nice = False
+            break
+        else:
+            index -= 1
 
-print(nice)
+    if nice == True:
+        ans += 1
+
+print(ans)
