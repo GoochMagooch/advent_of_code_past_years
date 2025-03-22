8aff5d4f368ddea86987365fd49983b3d7af5d5a
Magooch <crvallecito@gmail.com>
2025-03-22 13:26:03 -0600

    Add functionality to check for repeating characters

--- 2015/day5/p1.py
+++ 2015/day5/p1.py
@@ -5,7 +5,7 @@ vowels = ['a', 'e', 'i', 'o', 'u']
 nice = 0
 for i in range(len(data)):
     count = 0
-
+    '''
     # Counts vowels in each string
     vowel_count = 0
     for j in data[i]:
@@ -13,5 +13,15 @@ for i in range(len(data)):
             vowel_count += 1
     if vowel_count >= 3:
         count += 1
-    
+    '''
+    # Checks if string contains at least one set of repeating characters
+    index = -1
+    same_char_count = 0
+    for j in range(len(data[i]) - 1):
+        if data[i][index] == data[i][index - 1]:
+            same_char_count += 1
+        index -= 1
+    if same_char_count > 0:
+        count += 1
+
 print(nice)
